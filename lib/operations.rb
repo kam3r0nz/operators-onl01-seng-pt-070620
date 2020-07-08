5ee0dd73f4edc13e85e70740f2e24f7f7f101ecf
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-08 23:33:37 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,17 @@
 def unsafe?(speed)
   if speed > 60
-    return "true"
+    return true
   elsif speed < 40
-    return "true"
+    return true
   else
-    return "false"
+    return false
   end
 end
 
 
 
 def not_safe?(speed)
-	speed > 60 ? "true":"false"
+	speed > 60 ? true:false
 end
 	
 
